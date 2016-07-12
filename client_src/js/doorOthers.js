b6b445fbb4a54426fb730ba715fc24223dfca926
AlexandrAnash <AAnashkin@ath.ru>
2016-07-12 22:35:14 +0300

    update

--- client_src/js/doorOthers.js
+++ client_src/js/doorOthers.js
@@ -70,15 +70,26 @@ function Door1(number, onUnlock) {
         this.popup.querySelector('.door-riddle__button_1'),
     ];
     buttons.forEach(function(b) {
-        b.addEventListener('pointerdown',   function(e) {console.log('event = pointerdown = ', e)}.bind(this));
-        //b.addEventListener('pointermove',   function(e) {console.log('event = pointermove = ', e)}.bind(this));
+        b.addEventListener('pointerdown', function(e) {
+            e.target.setPointerCapture(e.pointerId);
+            e.target.style.opacity = .5;
+            e.target.style.borderColor = 'red';
+            e.target.style.top = e.y + 'px';
+            e.target.style.position = 'absolute';
+            
+            console.log('event = pointerdown = ', e);
+            console.log('this.popup = ', this.popup);
+        }.bind(this));
+        b.addEventListener('pointermove',   function(e) {
+            e.target.style.top = e.y + 'px';
+        }.bind(this));
         b.addEventListener('intercancel',   function(e) {console.log('event = intercancel = ', e)}.bind(this));
         b.addEventListener('pointercancel', function(e) {console.log('event = pointercancel = ', e)}.bind(this));
         b.addEventListener('pointerover',   function(e) {console.log('event = pointerover = ', e)}.bind(this));
         b.addEventListener('pointerout',    function(e) {console.log('event = pointerout = ', e)}.bind(this));
         b.addEventListener('pointerenter',  function(e) {console.log('event = pointerenter = ', e)}.bind(this));
         b.addEventListener('pointerleave',  function(e) {console.log('event = pointerleave = ', e)}.bind(this));
-    });
+    }.bind(this));
     // this.popup.addEventListener('click', function() {
     //     this.unlock();
     // }.bind(this));
